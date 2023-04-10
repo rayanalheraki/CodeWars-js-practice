91a5d0ed1eb793b5941fa75687ceeaf885b8c54d
RAYAN ALHERAKI <rayyanalhiraki3@gmail.com>
2023-04-11 02:55:38 +0300

    edit getLikesMessageText

--- getLikesMessageText.ts
+++ getLikesMessageText.ts
@@ -1,13 +1,12 @@
-function getLikesMessageText(names) {
+function likes(names) {
   const arrLength = names.length;
   if (arrLength === 0) return "no one likes this";
   if (arrLength < 2) return `${names[0]} likes this`;
   if (arrLength < 3) return `${names[0]} and ${names[1]} like this`;
   if (arrLength < 4)
     return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-  if (arrLength >= 4) {
-    return `${names[0]}, ${names[1]} and ${arrLength - 2} others like this`;
-  }
+
+  return `${names[0]}, ${names[1]} and ${arrLength - 2} others like this`;
 }
 
 /**
